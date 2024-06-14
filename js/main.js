d70e36f2f65f00decf79339e002dd14f546fe8cb
Muhammad Asim Siddiqui <129016875+muhammadasimsiddiqui@users.noreply.github.com>
2024-06-14 17:28:43 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 50) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+};
+
+// Nav Hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
+
+// Contact Form
+function validate() {
+  alert("Form is being submitted.");
+}
